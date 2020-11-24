2b60d818edfd404a7e41ef9df916af71047a0423
Evgeniy Chernyshenko <magdegaben@gmail.com>
2020-11-24 23:26:38 +0200

    after fixing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import css from './styles.css';
+import css from "./css/styles.css";
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,57 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-const WebpackBar = require('webpackbar');
+const path = require("path");
+const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const WebpackBar = require("webpackbar");
 
-const loadModeConfigs = (env) => {
-    require(`./build-utils/${env.mode}.config`)(env);
-};
+const loadModeConfig = (env) =>
+  require(`./build-utils/${env.mode}.config.js`)(env);
 
-module.exports = (env) => merge({
-    mode: env.mode,
-    context: path.resolve(__dirname, "src"),
-    entry: './index.js',
-    output: {
+module.exports = (env) =>
+  merge(
+    {
+      mode: env.mode,
+      context: path.resolve(__dirname, "src"),
+
+      entry: "./index.js",
+      output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "[name].bundle.js"
-    },
-    module: {
-        rules: [{
+        filename: "[name].bundle.js",
+      },
+      module: {
+        rules: [
+          {
             test: /\.js$/,
-            exclude: /node_module/,
-            use: ["babel-loader"]
-        },
-            {
+            exclude: /node_modules/,
+            use: ["babel-loader"],
+          },
+          {
+            test: /\.html$/,
+            use: ["html-loader"],
+          },
+          {
             test: /\.(gif|png|jpe?g|svg)$/,
-            use: [{
-                    loader: "url-loader",
-                    option: {
-                        name: "[path]/[name].[ext]",
-                        limit: false,
-                    }
-                }]
-            },
-            {
-                test: /\.html$/,
-                use:["html-loader"]
-            },
-            {
-                test:/\hbs$/,
-                use:["handlebars-loader"]
-            }
+            use: [
+              {
+                loader: "url-loader",
+                options: {
+                  name: "[path]/[name].[ext]",
+                  limit: false,
+                },
+              },
+            ],
+          },
+          {
+            test: /\.hbs$/,
+            use: ["handlebars-loader"],
+          },
         ],
-    },
-    plugins: [
+      },
+      plugins: [
         new CleanWebpackPlugin(),
         new FriendlyErrorsWebpackPlugin(),
         new WebpackBar(),
-        ]
-},
-    loadModeConfigs(env)
-)
+      ],
+    },
+    loadModeConfig(env),
+  );
