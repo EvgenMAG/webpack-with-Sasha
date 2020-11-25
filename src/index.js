7ab8f5aee6912f505e859848b3c6f63efacc0157
Evgeniy Chernyshenko <magdegaben@gmail.com>
2020-11-25 13:11:28 +0200

    edwfwef

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import css from "./css/styles.css";
+
+class User {
+  constructor(name) {
+  this._name = name
+}
+  get name() {
+   return this._name
+ }
+}
+const mango = new User('MANGO')
+console.log(mango.name);
